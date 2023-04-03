4fb9b5b8e5067fe02b9b0028498b5c39bfc269ea
usulemre <emreusul83@gmail.com>
2023-04-03 05:01:40 +0300

    first 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 // app.use(express.static("public/"));
 
-app.get("/", (req, res) => res.send("Hello World Version 1"));
+app.get("/", (req, res) => res.send("Hello World Version 2"));
 app.listen(8080),
   function () {
     console.log("server listining at 8080 ");
